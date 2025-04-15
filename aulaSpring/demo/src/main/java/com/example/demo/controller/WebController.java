48e3c07e3cb29928246d7dd0e698788143214253
MauricioHS91 <145577830+MauricioHS91@users.noreply.github.com>
2025-04-15 00:53:40 +0000

    2025-04-14

---
+++ aulaSpring/demo/src/main/java/com/example/demo/controller/WebController.java
@@ -0,0 +1,16 @@
+package com.exemple.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController{
+    @GetMapping("/")
+    public String home (Model model){
+        model.addAttribute("message", "Olá Mundo!");
+        return "home";
+
+    }
+
+}./
